b4784522d28a1c7ba5efbd409f42e0aac09406c0
Firmiana <firmiana@Firmiana-MacBook-Air.local>
2025-03-18 14:37:28 +0800

    1

--- field.cpp
+++ field.cpp
@@ -9,7 +9,26 @@ Field::Field(int nx, int ny) : nx(nx), ny(ny), Ex(nx * ny, 0.0), Ey(nx * ny, 0.0
 
 double Field::getEx(int i, int j) const
 {
+    checkBounds(i, j);
     return Ex[i * ny + j];
+}
+
+double Field::getEy(int i, int j) const
+{
+    checkBounds(i, j);
+    return Ey[i * ny + j];
+}
+
+double Field::getBz(int i, int j) const
+{
+    checkBounds(i, j);
+    return Bz[i * ny + j];
+}
+
+void Field::setEx(int i, int j, double new_Ex)
+{
+    checkBounds(i, j);
+    Ex[i * ny + j] = new_Ex;
 }
 
 void Field::checkBounds(int i, int j) const
